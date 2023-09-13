09a66701313e8871fa0df32b08d33c3c11702927
momeemt <momiimt@gmail.com>
2023-09-13 19:18:15 +0900

    add: coco dns

---
+++ dns.tf
@@ -0,0 +1,17 @@
+resource "cloudflare_record" "frontend" {
+  name    = "coco"
+  proxied = false
+  ttl     = 1
+  type    = "CNAME"
+  value   = "cname.vercel-dns.com"
+  zone_id = var.cloudflare_zone_id
+}
+
+resource "cloudflare_record" "backend" {
+  name = "api.coco"
+  proxied = false
+  ttl = 1
+  type = "A"
+  value = azurerm_public_ip.public_ip.ip_address
+  zone_id = var.cloudflare_zone_id
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "coco_vm_ssh_public_key" {
   type = string
 }
+
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
