61d39106459806b6de7090bf5525d0f0534d458e
momeemt <momiimt@gmail.com>
2023-09-13 15:06:00 +0900

    add: azure storage container for coco

---
+++ storage.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "storage" {
+    name = "coco${random_id.random_id.hex}"
+    resource_group_name = azurerm_resource_group.region.name
+    location = azurerm_resource_group.region.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+    name = "coco${random_id.random_id.hex}"
+    storage_account_name = azurerm_storage_account.storage.name
+    container_access_type = "blob"
+}
