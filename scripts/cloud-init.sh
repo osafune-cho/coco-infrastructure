beafcfc1b0f79af7224780df5a021e2506549464
momeemt <momiimt@gmail.com>
2023-09-14 00:08:37 +0900

    add: DB_SSLMODE environment variable

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -8,6 +8,7 @@ set -eou pipefail
     echo "export DB_PASSWORD=${DB_PASSWORD}"
     echo "export DB_NAME=${DB_NAME}"
     echo "export DB_PORT=${DB_PORT}"
+    echo "export DB_SSLMODE=require"
     echo "export AZURE_STORAGE_ACCOUNT_NAME=${AZURE_STORAGE_ACCOUNT_NAME}"
     echo "export AZURE_STORAGE_ACCOUNT_KEY=${AZURE_STORAGE_ACCOUNT_KEY}"
     echo "export AZURE_STORAGE_CONTAINER_NAME=${AZURE_STORAGE_CONTAINER_NAME}"
